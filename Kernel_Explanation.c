8c5863b27673f3820cd773c83305a26e8863b2b7
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-09-25 07:32:25 -0400

    Add kernel function to apply force in simulation

---
+++ Kernel_Explanation.c
@@ -0,0 +1,10 @@
+__kernel void apply_force(__global float *pos_y,
+                          __global float *vel_y,
+                          float g,
+                          int n) {
+    int i = get_global_id(0);
+    if (i < n) {
+        vel_y[i] += g;        // update velocity with gravity
+        pos_y[i] += vel_y[i]; // integrate position
+    }
+}
